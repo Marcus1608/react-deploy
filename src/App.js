5ecddcfe43f340105d3f12c2c2db2b0ee53eff8f
Marcus1608 <markpc1608@gmail.com>
2021-03-06 11:27:16 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>My React App</h1>
+   <h2>Mark Corbin</h2>
+   <img src={logo} alt=""/>
+    </div>
+  );
+}
+
+export default App;
